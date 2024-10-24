400ba58ae37b80bc0e2bbaac48c42eea43f14c6a
kosteleyyv <72693294+kosteleyyv@users.noreply.github.com>
2024-10-24 21:27:16 +0700

    Delete ClipBoardTextEditer directory

--- ClipBoardTextEditer/ClipBoardTextEditer/RemovingDoubleWhitespace.cpp
+++
@@ -1,7 +0,0 @@
-#include "RemovingDoubleWhitespace.h"
-
- string RemovingDoubleWhitespace::getEditionResult() const {
-	string copy = text; // копируем значение поля базового класса
-	replace(copy, "  ", " "); // испольуем метод базового класса  для замены двух пробелов на один
-	return copy;
-}
