ffbb06e754f8aa822c507bbc5ae12569aef0988b
kosteleyyv <72693294+kosteleyyv@users.noreply.github.com>
2024-10-24 21:27:53 +0700

    Add files via upload

---
+++ ClipBoardTextEditer/ClipBoardTextEditer/Application.cpp
@@ -0,0 +1,51 @@
+﻿#include "Application.h"
+
+Application::Modes Application::Mode = Application::Modes::TransliteKeyboardLayoutRusToEng;
+
+ int Application::run() {
+
+	setlocale(LC_ALL, "Russian");
+	try {
+		// извлекаем текст из буфера обмена
+		string stringFromClipboard = Clipboard::getText();
+
+		// определяем нужный объект для обработки по режиму запуска
+		TextEditer* textEditer = getTextEditer();
+
+		// передаем в него строку из буфера
+		textEditer->setSourceText(stringFromClipboard);
+
+		// получаем результат обработки
+		string stringToClipboard = textEditer->getEditionResult();
+
+		// возвращаем преобразованную строку в буфер обмена
+		Clipboard::setText(stringToClipboard.c_str());
+
+		cout << "Строка из буфера обмена: " << stringFromClipboard.c_str() << endl << endl;
+		cout << "Строка в буфере обмена:  " << stringToClipboard.c_str() << endl;
+	}
+	catch (exception exp) {
+		cout << "Ошибка: " << exp.what() << endl;
+	}
+
+	system("pause");
+	return 0;
+}
+
+ TextEditer* Application::getTextEditer() {
+
+	switch (Mode) {
+	case Application::Modes::RemovingDoubleWhitespace:
+		return new RemovingDoubleWhitespace();
+	case Application::Modes::TransliteKeyboardLayoutRusToEng:
+		return new TransliteKeyboardLayoutRusToEng(BaseTransliteTextRusToEng::TranslationDirection::RusToEng);
+	case Application::Modes::TransliterationRusToEng:
+		return new TransliterationRusToEng(BaseTransliteTextRusToEng::TranslationDirection::RusToEng);
+	case Application::Modes::TransliteKeyboardLayoutEngToRus:
+		return new TransliteKeyboardLayoutRusToEng(BaseTransliteTextRusToEng::TranslationDirection::EngToRus);
+	case Application::Modes::TransliterationEngToRus:
+		return new TransliterationRusToEng(BaseTransliteTextRusToEng::TranslationDirection::EngToRus);
+	default:
+		return nullptr;
+	}
+}
---
+++ ClipBoardTextEditer/ClipBoardTextEditer/TransliteKeyboardLayoutRusToEng.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include "BaseTransliteTextRusToEng.h"
+
+/// перевод раскладки с кириллицы на латиницу и наоборот
+class TransliteKeyboardLayoutRusToEng : public BaseTransliteTextRusToEng {
+
+	/// длина алфавит перевода
+	static const int TRANSLITE_LAYOUT_SYMBOLS_COUNT = 95;
+
+	/// алфавит перевода
+	static const char* const transliteLayoutDictionary[TRANSLITE_LAYOUT_SYMBOLS_COUNT][2];
+
+protected:
+	// переопределяемые методы, которые возвращают нужную таблицу алфавита
+
+	virtual int getDictionaryLenght() const override;
+	virtual const char* const getSymbol(int index, DictionaryKeyTypes key) const override;
+
+public:
+	TransliteKeyboardLayoutRusToEng(TranslationDirection type);
+};
+
+
